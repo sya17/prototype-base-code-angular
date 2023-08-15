7edcd3b3460bcf2a6c577964f1cd51df1620f800
Sarip Hidayatullah <102409123+sya17@users.noreply.github.com>
2023-08-15 22:43:54 +0700

    fix:workflow group:detail

--- src/app/theme/components/custom-input/custom-input.component.ts
+++ src/app/theme/components/custom-input/custom-input.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { FormControlName } from '@angular/forms';
+import { FormControlName, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
-  styleUrls: ['./custom-input.component.scss']
+  styleUrls: ['./custom-input.component.scss'],
 })
 export class CustomInputComponent {
   @Input() placeholder: string = '';
-  @Input() controlName = FormControlName.name;
+  @Input() controlName: string = '';
+  @Input() parentForm: FormGroup | undefined;
 }
